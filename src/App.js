8cadce9b43c33fa12f3f1e7278a7f9977cb32e4f
Ausmanow <adel.usmanoff@yandex.ru>
2022-12-06 17:43:52 +0300

    add react project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+// import React from 'react';
+// import './App.css';
+// import {MainPage} from './components/MainPage/MainPage';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//       path: '/',
+//       element: <MainPage />,
+//   }
+// ]);
+
+// export const App = () => {
+//   return (
+//     <RouterProvider router={router} />
+//   )
+// }
 import React from 'react';
 import './App.css';
 import {MainPage} from './components/MainPage/MainPage';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter, Route, Switch
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-      path: '/',
-      element: <MainPage />,
-  }
-]);
 
 export const App = () => {
   return (
-    <RouterProvider router={router} />
+<BrowserRouter basename='/4p22-final-project-adel-usmanov/'>
+  <Switch>
+    <Route path='/' exact>
+      <MainPage />
+    </Route>
+  </Switch>
+</BrowserRouter>
+  
   )
 }
 
